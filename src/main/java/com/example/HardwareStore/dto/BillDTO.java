5284afccd7c04326f07da185b57eb2ec6f05ec04
Esteban Carvajal <johan.gal.car@gmail.com>
2022-06-12 17:23:52 -0500

    CreateBillUseCaseTest

--- src/main/java/com/example/HardwareStore/dto/BillDTO.java
+++ src/main/java/com/example/HardwareStore/dto/BillDTO.java
@@ -1,20 +1,26 @@
 package com.example.HardwareStore.dto;
 
 import com.example.HardwareStore.collection.Product;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
-@Document(collection="bill")
+@AllArgsConstructor
 public class BillDTO {
 
     private String id;
+    private String date;
     private String clientName;
     private String sellerName;
     private Integer productsSold;
     private Integer totalSale;
+
+    public BillDTO() {
+
+    }
 
 }
 
---
+++ src/test/java/com/example/HardwareStore/usescases/CreateBillUseCaseTest.java
@@ -0,0 +1,44 @@
+package com.example.HardwareStore.usescases;
+
+import com.example.HardwareStore.collection.Bill;
+import com.example.HardwareStore.dto.BillDTO;
+import com.example.HardwareStore.repository.BillRepository;
+import com.example.HardwareStore.usecases.CreateBillUseCase;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class CreateBillUseCaseTest {
+
+    @MockBean
+    private CreateBillUseCase createBillUseCase;
+
+    @Mock
+    BillRepository billRepository;
+
+    @Test
+    public void setCreateBillUseCase() {
+
+        Bill bill = new Bill (
+                "X",
+                "X",
+                "X",
+                "X",
+                 7,
+                100
+        );
+
+        BillDTO billDTO = new BillDTO();
+
+        billDTO.setDate(bill.getDate());
+        billDTO.setClientName(bill.getClientName());
+        billDTO.setClientName(bill.getClientName());
+        billDTO.setSellerName(bill.getSellerName());
+        billDTO.setTotalSale(bill.getTotalSale());
+
+
+
+    }
+}
